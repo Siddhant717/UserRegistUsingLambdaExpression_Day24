9bc620248f19106251cbdbaa5d7623acd37ed006
Siddhant <santoshshrivastava39@gmail.com>
2022-07-17 19:25:29 +0530

    Valid First Name

---
+++ RegexUsingLambdaExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexUsingLambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration.CheckFirstName();
+        }
+    }
+}
